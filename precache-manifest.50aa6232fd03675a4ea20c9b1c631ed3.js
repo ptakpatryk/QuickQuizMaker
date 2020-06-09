d5f81f5b23d57001230e1b349a6813e61562d54a
Patryk Ptak <ptaszqq@gmail.com>
2020-06-09 13:57:10 +0100

    Updates

---
+++ precache-manifest.50aa6232fd03675a4ea20c9b1c631ed3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4547d183dea8877ae5504ae8e50b0b2b",
+    "url": "/QuickQuizMaker/index.html"
+  },
+  {
+    "revision": "456f3d51d0cac15beaac",
+    "url": "/QuickQuizMaker/static/css/2.14f3cd7d.chunk.css"
+  },
+  {
+    "revision": "a9a3b7e19e394c96c2c7",
+    "url": "/QuickQuizMaker/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "456f3d51d0cac15beaac",
+    "url": "/QuickQuizMaker/static/js/2.b78b94a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/QuickQuizMaker/static/js/2.b78b94a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a3b7e19e394c96c2c7",
+    "url": "/QuickQuizMaker/static/js/main.6897f214.chunk.js"
+  },
+  {
+    "revision": "37c93d1834d4ea8ec093",
+    "url": "/QuickQuizMaker/static/js/runtime-main.23b8e12f.js"
+  },
+  {
+    "revision": "95937f9f3818da9a8993642b27d07f2f",
+    "url": "/QuickQuizMaker/static/media/arrow-go.95937f9f.svg"
+  },
+  {
+    "revision": "b0b7604f34db2b1df8f01fb264a9b252",
+    "url": "/QuickQuizMaker/static/media/authForAccess.b0b7604f.svg"
+  },
+  {
+    "revision": "3d9575fbe92a5bdd8cac736e99abcdd9",
+    "url": "/QuickQuizMaker/static/media/delete.3d9575fb.svg"
+  },
+  {
+    "revision": "2aca2b5e260b27f5d43d57363a721e22",
+    "url": "/QuickQuizMaker/static/media/done.2aca2b5e.svg"
+  },
+  {
+    "revision": "026ce7a0a6c16f72063fc8b57da10698",
+    "url": "/QuickQuizMaker/static/media/edit.026ce7a0.svg"
+  },
+  {
+    "revision": "642786ec65b708c04ba407c90ba90fe1",
+    "url": "/QuickQuizMaker/static/media/logo.642786ec.svg"
+  },
+  {
+    "revision": "54efb753081a566540e827c891aea05c",
+    "url": "/QuickQuizMaker/static/media/select_arrow_down.54efb753.svg"
+  },
+  {
+    "revision": "55f4dc9c461a053c8f0c71f9400717b4",
+    "url": "/QuickQuizMaker/static/media/thatsAllText.55f4dc9c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.236de55c8997446eac46c76ce53abec2.js"
+  "/QuickQuizMaker/precache-manifest.50aa6232fd03675a4ea20c9b1c631ed3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/QuickQuizMaker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

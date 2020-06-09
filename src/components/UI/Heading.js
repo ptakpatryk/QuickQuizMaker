6f1596886be269b99f6f7d95ee8ec5f2614b354c
Patryk Ptak <ptaszqq@gmail.com>
2020-06-09 13:31:41 +0100

    build gh-pages

--- src/components/UI/Heading.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Heading = styled.h1`
-  font-weight: ${({ theme }) => theme.bold};
-  font-size: ${({ theme }) => theme.textSize.xl};
-`;
-
-export default Heading;
--- src/components/UI/Paragraph.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Paragraph = styled.h3`
-  font-weight: ${({ theme }) => theme.light};
-  font-size: ${({ theme }) => theme.textSize.m};
-  margin-bottom: 30px;
-`;
-
-export default Paragraph;
